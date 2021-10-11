a524381c0377a847036e4761161fa5e3b14d14ff
Lisa Cormier <lcormier5@hotmail.com>
2021-10-11 22:36:50 +0000

    add exercise 6

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -8,3 +8,6 @@ puts "----------"
 @store2 = Store.find_by(id: 2)
 @store1.annual_revenue = 200000
 @store1.save
+@store4 = Store.find_by(id: 4)
+@store5 = Store.find_by(id: 5)
+@store6 = Store.find_by(id: 6)
