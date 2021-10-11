cfc5c22926ada33622c2c722dbd236dab8042f51
Lisa Cormier <lcormier5@hotmail.com>
2021-10-11 21:12:27 +0000

    add exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "total stores revenue: $#{Store.sum(:annual_revenue)}"
+puts "average total store revenue: $#{Store.average(:annual_revenue)}"
+puts "number of stores generating more that $1M in revenue: #{Store.where("annual_revenue >= ?", 1000000).count}"
